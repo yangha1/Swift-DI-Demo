1136f4a09a93b29a2c8ddeacc0d9ead9fecb5f78
yanghai <yanghai@tngdigital.com.my>
2022-08-01 16:35:41 +0800

    Initial Commit

---
+++ Homie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homie
+//
+//  Created by Harvey on 2022/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
